aac95af7b9b5b2a691455b0b3413645582406024
Het Patel <hetpatel1b@gmail.com>
2026-03-30 14:09:38 +0530

    New Methods to access Privite Attribute

---
+++ OPP/Getter_and_Setter.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+    string name;
+    float cgpa;
+
+public:
+    void setName(string s_name)
+    {
+        name = s_name;
+    }
+
+    void setCgpa(float s_cgpa)
+    {
+        cgpa = s_cgpa;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    float getCgpa()
+    {
+        return cgpa;
+    }
+
+    void getpercentage()
+    {
+        cout << (cgpa * 10) << endl;
+    }
+};
+
+int main()
+{
+
+    Student s1;
+
+    s1.setName("Het Patel");
+    cout << s1.getName()<<endl;
+
+    s1.setCgpa(8.39);
+    cout << s1.getCgpa()<<endl;
+
+    return 0;
+}
