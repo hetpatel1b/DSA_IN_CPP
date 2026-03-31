979bc471f1384146b4a709e98cba5fd1f5f8f474
Het Patel <hetpatel1b@gmail.com>
2026-03-31 13:37:12 +0530

    CopyConstructor Is Completed

---
+++ OPP/CopyConstructor.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Car
+{
+public:
+    string carname;
+    string carcolor;
+
+    Car(string name, string color)
+    {
+        carname = name;
+        carcolor = color;
+        cout << "Constructor With Parameters Which is Automatically Called By Variable" << endl;
+    }
+
+    Car(Car &orginal)
+    {
+        cout << "Copying..." << endl;
+        carname = orginal.carname;
+        carcolor = orginal.carcolor;
+    }
+};
+
+int main()
+{
+    Car c1("BMW M4", "Black");
+
+    Car c2(c1);
+
+    cout << c2.carname << endl;
+    cout << c2.carcolor << endl;
+    return 0;
+}
