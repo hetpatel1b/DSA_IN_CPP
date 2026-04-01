750ce724b4b64d1669f1a5f941d3b18091c70d8f
Het Patel <hetpatel1b@gmail.com>
2026-04-01 08:26:04 +0530

    Destructor is Completed

---
+++ OPP/Destructor.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Car
+{
+public:
+    string carname;
+    string carcolor;
+    int *mileage;
+
+    Car(string name, string color)
+    {
+        carname = name;
+        carcolor = color;
+        cout << "Constructor With Parameters Which is Automatically Called By Variable" << endl;
+        mileage = new int;
+        *mileage = 12;
+    }
+
+    Car(Car &orginal)
+    {
+        cout << "Copying..." << endl;
+        carname = orginal.carname;
+        carcolor = orginal.carcolor;
+        mileage = new int;
+        *mileage = *orginal.mileage;
+    }
+    ~Car()
+    {
+        cout << "Object Deleting..." << endl;
+        if (mileage != nullptr)
+        {
+            delete mileage;
+            mileage = NULL;
+        }
+    }
+};
+
+int main()
+{
+    Car c1("BMW M4", "Black");
+
+    Car c2(c1);
+
+    cout << c2.carname << endl;
+    cout << c2.carcolor << endl;
+    cout << *c2.mileage << endl;
+    return 0;
+}
