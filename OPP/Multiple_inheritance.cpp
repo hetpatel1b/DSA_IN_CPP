3aa8d241b7424c79fa2bd98777eeb1e6dd0884e6
Het Patel <hetpatel1b@gmail.com>
2026-04-01 20:05:53 +0530

    MultipleInheritance Is Completed

---
+++ OPP/Multiple_inheritance.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// First base class
+class Teacher
+{
+public:
+    int salary;
+    string subject;
+};
+
+// Second base class
+class Student
+{
+public:
+    int roll_no;
+    float cgpa;
+};
+
+// Multiple inheritance: TA inherits from both Student and Teacher
+class TA : public Student, public Teacher
+{
+public:
+    string name;
+};
+
+int main()
+{
+    // TA object can access members of both parent classes
+    TA ta1;
+    ta1.name = "Het Patel";
+    ta1.subject = "DSA";
+    ta1.cgpa = 8.39;
+
+    cout << ta1.name << endl;
+    cout << ta1.subject << endl;
+    cout << ta1.cgpa << endl;
+
+    return 0;
+}
