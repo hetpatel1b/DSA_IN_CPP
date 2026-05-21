9bf1ab6e3cfff0b8242073a4d768899a3f5a60ee
Het Patel <hetpatel1b@gmail.com>
2026-05-21 22:27:21 +0530

    Push Back

---
+++ Linked LIst/Push_back.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    int data;
+
+public:
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head = NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+};
+
+int main()
+{
+    List ll;
+    
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+
+
+    return 0;
+}
