d3cc554facca0f9b4747404c4a959f848c40e0de
Het Patel <hetpatel1b@gmail.com>
2026-05-22 08:34:44 +0530

    Pop Front

---
+++ Linked LIst/Pop_front.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+
+    ~Node()
+    {
+        if (next != NULL)
+        {
+            delete next;
+            next = NULL;
+        }
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    ~List()
+    {
+        if (head != NULL)
+        {
+            delete head;
+            head = NULL;
+        }
+    }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void insertList(int val, int pos)
+    {
+        Node *newNode = new Node(val);
+
+        Node *temp = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    void pop_front()
+    {
+        if (head == NULL)
+        {
+            cout << "LL is empty\n";
+            return;
+        }
+        Node *temp = head;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            if (temp == NULL)
+            {
+                cout << "Position is Invalid" << endl;
+                return;
+            }
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "Null\n";
+    }
+};
+
+int main()
+{
+    List ll;
+
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.printList();
+
+    ll.pop_front();
+
+    ll.printList();
+    return 0;
+}
