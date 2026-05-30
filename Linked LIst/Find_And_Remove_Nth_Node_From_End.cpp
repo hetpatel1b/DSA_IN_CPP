24a5ba465608e43cfdb5d3d2e5d0cf1a637a92ef
Het Patel <hetpatel1b@gmail.com>
2026-05-30 13:35:58 +0530

    Find_And_Remove_Nth_Node_From_End

---
+++ Linked LIst/Find_And_Remove_Nth_Node_From_End.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    int findSize()
+    {
+        int sz = 0;
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            temp = temp->next;
+            sz++;
+        }
+        return sz;
+    }
+
+    void removeNth_end(int n)
+    {
+        int size = findSize();
+        Node *prev = head;
+
+        for (int i = 1; i < size - n; i++)
+        {
+            prev = prev->next;
+        }
+        Node *Del = prev->next;
+        
+        cout << "Going To Delete :" << Del->data << endl;
+        prev->next = prev->next->next;
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "Null" << endl;
+    }
+};
+
+int main()
+{
+    List ll;
+
+    ll.push_front(4);
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.printList();
+
+    ll.removeNth_end(2);
+
+    ll.printList();
+
+    return 0;
+}
