3fa05c7c431bf139167dce0041b6fd469d9a0a54
Het Patel <hetpatel1b@gmail.com>
2026-06-02 09:41:43 +0530

    Detect_Cycle

---
+++ Linked LIst/Detect_Cycle.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+public:
+    Node *head;
+    Node *tail;
+
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    bool isCycle(Node *head)
+    {
+        Node *slow = head;
+        Node *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+            {
+                cout << "Cycle Exists" << endl;
+                return true;
+            }
+        }
+        cout << "Cycle Not Exists" << endl;
+        return false;
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "Null";
+    }
+};
+
+int main()
+{
+    List ll;
+
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.tail->next = ll.head;
+
+    ll.isCycle(ll.head);
+    return 0;
+}
