264378235e99aa301aa577a9b1c6cd231e4aa905
Het Patel <hetpatel1b@gmail.com>
2026-06-18 20:04:34 +0530

    Stack using Linked List

---
+++ Stack/Stack_Using_LInkedList.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class T>
+class Stack
+{
+    list<T> ll;
+
+public:
+    void push(T val)
+    {
+        ll.push_front(val);
+    }
+    void pop()
+    {
+        ll.pop_front();
+    }
+    T top()
+    {
+        return ll.front();
+    }
+    bool isEmpty()
+    {
+        return ll.size() == 0;
+    }
+};
+
+int main()
+{
+    Stack<int> s;
+    s.push(3);
+    s.push(2);
+    s.push(1);
+
+    while (!s.isEmpty())
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
+    bool ans = s.isEmpty();
+    cout << ans << endl;
+
+    return 0;
+}
