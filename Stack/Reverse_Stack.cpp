aea420bbf88638d63e26889c3df1e24a5f015ac8
Het Patel <hetpatel1b@gmail.com>
2026-06-22 08:59:27 +0530

    Reverse Stack

---
+++ Stack/Reverse_Stack.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pushAtBottom(stack<int> &s, int val)
+{
+    if (s.empty() == 1)
+    {
+        s.push(val);
+        return;
+    }
+
+    int temp = s.top();
+    s.pop();
+    pushAtBottom(s, val);
+    s.push(temp);
+}
+
+void reverse(stack<int> &s){
+    if(s.empty()){
+        return;
+    }
+    int temp=s.top();
+    s.pop();
+    reverse(s);
+    pushAtBottom(s,temp);
+}
+
+void printStack(stack<int> s){
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<< "\n";
+}
+
+int main()
+{
+    stack<int> s;
+
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    s.push(0);
+    printStack(s);
+
+    reverse(s);
+
+    printStack(s);
+
+    return 0;
+}
